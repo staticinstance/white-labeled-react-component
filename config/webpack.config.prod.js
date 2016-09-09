8d36a0b2e79d65025379261a92cbb404985dcbdd
Michael Bradley <mbradleyis@gmail.com>
2016-09-08 21:14:14 -0700

    add chunk name

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -46,7 +46,7 @@ module.exports = {
     // There will be one main bundle, and one file per asynchronous chunk.
     // We don't currently advertise code splitting but Webpack supports it.
     filename: 'index.js',
-    //chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
     // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath,
     library: 'white-labeled-react-component',
